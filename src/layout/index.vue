78717f2aadb823029214974996e8ab9eb1e3d635
choojoonbum <cjb3333@naver.com>
2025-07-10 10:41:38 +0900

    기본레이아웃, 해더영역 적용

---
+++ src/layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="wrap">
+    <h1 class="blind">웹사이트 제목</h1>
+    <Header/>
+    <section id="container">
+      <router-view/>
+    </section>
+    <Footer/>
+  </section>
+</template>
+<script>
+import Header from "@/layout/Header";
+import Footer from "@/layout/Footer";
+export default {
+  components: {Footer, Header}
+}
+</script>
