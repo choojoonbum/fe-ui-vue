d7dae5395d8b66cddd457aa191cfaa3f08ddac35
choojoonbum <cjb3333@naver.com>
2025-07-14 12:02:30 +0900

    서브페이지 레이아웃 구성

---
+++ src/views/Books.vue
@@ -0,0 +1,13 @@
+<template>
+  <subpage :pagetit="'도서 소개'">
+    <div class="bookpage">도서 소개</div>
+  </subpage>
+</template>
+<script>
+import Subpage from "@/layout/components/Subpage.vue";
+export default {
+  components: {
+    Subpage,
+  }
+}
+</script>
